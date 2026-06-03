409a69980bdfe1e53f36593ae84cf42aa9fb4c57
guralevicartem933-a11y <guralevicartem933@gmail.com>
2026-06-03 09:45:17 +0300

    Update ChestShopPlugin.java

--- src/main/java/me/yourname/chestshop/ChestShopPlugin.java
+++ src/main/java/me/yourname/chestshop/ChestShopPlugin.java
@@ -19,7 +19,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.AsyncChatEvent;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
